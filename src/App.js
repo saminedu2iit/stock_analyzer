392944c4226a7267d93daef31cb9d36a6610de3d
saminedu2iit <68290030+saminedu2iit@users.noreply.github.com>
2022-03-23 20:30:00 +0530

    fff

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
 
           </Route>
 
-          <Route exact path="/home" render={() => {
+          <Route exact path="/stock_analyzer" render={() => {
             return (
               <>
               {/* <Navbar /> */}
